b2b8523eb003deb190fabfae5297136ee5c9ff67
Isac Gansmoe <isac@gansmoe.se>
2021-11-01 14:16:34 +0100

    first commit

---
+++ routes/city.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+app.get("/", (req, res) => {
+    fs.readFile('./data/city.json', (err, data) => {
+
+        if (err) {
+            console.log(err);
+            res.status(400).json({success: false});
+            return;
+        }
+        res.send(data);
+    });
+});
+
+module.exports = router;
