6f41a5e524491f0f439e417de370477f3cfe4ebd
Isac Gansmoe <isac@gansmoe.se>
2021-11-01 14:16:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var countryRouter = require('./routes/country');
+var cityRouter = require('./routes/city');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/city', cityRouter);
+app.use('/contry', countryRouter);
+
+module.exports = app;
