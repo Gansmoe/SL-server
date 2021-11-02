f0104838734890190d45598ff3313dfc2bb318f6
Isac Gansmoe <isac@gansmoe.se>
2021-11-02 14:11:40 +0100

    Changes

---
+++ public/javascripts/script.js
@@ -0,0 +1,23 @@
+const addCountry = async () => {
+    const input = document.getElementById("countryInput");
+    const response1 = await fetch('https://hello-heroku-gansmoe.herokuapp.com/country');
+    const data1 = await response1.json();
+    const nextId = data1.length + 1;
+    const country = { id: nextId, countryname: input.value }
+
+    const response = await fetch('https://hello-heroku-gansmoe.herokuapp.com/country', {
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(country),
+    })
+    const data = await response.json();
+    console.log(data);
+    if (data.success) {
+        console.log("Country added!");
+    }
+};
+
+const addCountryBtn = document.getElementById('addCountryBtn');
+addCountryBtn.addEventListener('click', addCountry);
--- routes/country.js
+++ routes/country.js
@@ -5,6 +5,27 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'land.json');
 
+router.post("/", (req, res) => {
+    fs.readFile("land.json", (err, data) => {
+
+        if (err) {
+            console.log(err);
+            res.status(400).json({success: false});
+            return;
+        }
+
+        const country = JSON.parse(data);
+        country.push(req.body);
+
+        fs.writeFile("land.json", JSON.stringify(country, null, 2), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        res.status(200).json({success: true});
+    });
+});
+
 router.get("/", (req, res, next) => {
     fs.readFile('land.json', (err, data) => {
 
