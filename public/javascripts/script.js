0356fc312eae7588e7fada8a14c5a01690422df2
Isac Gansmoe <isac@gansmoe.se>
2021-11-02 15:06:37 +0100

    Add city function added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -20,5 +20,49 @@ const addCountry = async () => {
     }
 };
 
+const addCity = async () => {
+    const city = document.getElementById("city");
+    const cityPopulation = document.getElementById("population");
+    const countryName = document.getElementById("countryName");
+
+    const responseCity = await fetch('https://hello-heroku-gansmoe.herokuapp.com/city');
+    const dataCity = await responseCity.json();
+    const responseCountry = await fetch('https://hello-heroku-gansmoe.herokuapp.com/country');
+    const dataCountry = await responseCountry.json();
+    const nextId = dataCity.lenght + 1;
+    let countryId = 0;
+
+    console.log(dataCity);
+    console.log(dataCountry);
+
+    for (let i = 0; i<dataCountry.length; i++) {
+        if (countryName.value === dataCountry[i].countryname){
+            countryId = dataCountry[i].id;
+        }
+    }
+    console.log(countryId);
+
+    const newCity = {id: nextId, stadname: city.value, countryid: countryId, population: cityPopulation.value}
+
+
+    const response = await fetch('https://hello-heroku-gansmoe.herokuapp.com/city', {
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newCity),
+    })
+    const data = await response.json();
+    console.log(data);
+    if (data.success) {
+        console.log("City added!");
+        countryName.value = "City added!";
+
+    }
+}
+
 const addCountryBtn = document.getElementById('addCountryBtn');
 addCountryBtn.addEventListener('click', addCountry);
+
+const addCityBtn = document.getElementById('addCityBtn');
+addCityBtn.addEventListener('click', addCity);
--- routes/city.js
+++ routes/city.js
@@ -5,6 +5,27 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'city.json');
 
+router.post("/", (req, res) => {
+    fs.readFile("stad.json", (err, data) => {
+
+        if (err) {
+            console.log(err);
+            res.status(400).json({success: false});
+            return;
+        }
+
+        const city = JSON.parse(data);
+        city.push(req.body);
+
+        fs.writeFile("land.json", JSON.stringify(city, null, 2), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        res.status(200).json({success: true});
+    });
+});
+
 router.get("/", (req, res, next) => {
     fs.readFile('stad.json', (err, data) => {
 
